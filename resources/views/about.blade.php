26c67b96669549da5fbe05514ef290fb919a4eb0
Abhinai <AP565@NJIT.EDU>
2019-03-27 18:56:58 -0400

    added second page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    About this homepage
+                    About this website
                 </div>
 
                 <div class="links">
